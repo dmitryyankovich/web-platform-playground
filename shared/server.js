0526e16d871c3a16644d020fc9622efed1a80112
Alexei Skachykhin <alexei.skachykhin@live.com>
2014-02-03 14:45:55 +0300

    Make start page open every time server module is being required.

--- shared/server.js
+++ shared/server.js
@@ -1,12 +1,23 @@
 ﻿var express = require('express');
 var path = require('path');
+var open = require('open');
+var url = require('url');
 
 var app = express()
     .use(express.static(path.join(__dirname, '..', 'samples')))
     .use(express.directory(path.join(__dirname, '..', 'samples')))
     .use(express.json());
 
-var httpServer = app.listen(8080);
+var httpServer = app.listen(8080, function () {
+    var startPageUrl = url.format({
+        protocol: 'http:',
+        hostname: 'localhost',
+        port: httpServer.address().port
+    });
+
+    open(startPageUrl);
+});
+
 
 module.exports = {
     app: app,
